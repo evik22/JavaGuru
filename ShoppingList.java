6459a6ab31786c6664147e086c751e12725691c2
evik22 <evik22@yandex.ru>
2016-03-04 10:18:20 +0200

    Three functions covered, two to go.

--- ShoppingList.java
+++ ShoppingList.java
@@ -28,67 +28,194 @@ milk
 bread
 >6
 Good buy!
+
+... 
+case ADD_ITEM: addItem();
+ switch (month) {
+            case 1:  monthString = "January";
+                     break;
+            case 2:  monthString = "February";
+                     break;
+            case 3:  monthString = "March";
+                     break;
+            case 4:  monthString = "April";
+                     break;
+            default: monthString = "Invalid month";
+                     break;
+        }
 */
 
+import java.io.BufferedReader;
+import java.nio.file.Files;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.io.*;
+import java.nio.file.Paths;
 
 public class ShoppingList {
     static Scanner in = new Scanner(System.in);
     static String shopList[] = new String[10];
-    static int position = 0; //to save products position in an array
+    static int position = 0;
+
+    public final static int ADD_ITEM = 1;
+    public final static int LIST_ITEMS = 2;
+    public final static int DELETE_ITEM = 3;
+    public final static int SAVE_LIST = 4;
+    public final static int LOAD_LIST = 5;
+    public final static int EXIT = 6;
 
     public static void main(String[] args) {
 
         System.out.println("This is a shopping list application. Enter your choice:");
-        System.out.println("1 - add an item");
-        System.out.println("2 - list items");
-        System.out.println("3 - delete an item");
-        System.out.println("4 - save list to file");
-        System.out.println("5 - load list from file");
-        System.out.println("6 - exit");
+        System.out.println(ADD_ITEM + " - add an item");
+        System.out.println(LIST_ITEMS + " - list items");
+        System.out.println(DELETE_ITEM + " - delete an item");
+        System.out.println(SAVE_LIST + " - save list to file");
+        System.out.println(LOAD_LIST + " - load list from file");
+        System.out.println(EXIT + " - exit");
 
         int current;
-        while ((current = in.nextInt()) != 0) { //1 - add an item
-            if (current == 1) {
-                System.out.println("Enter item to add: ");
-                String item = in.nextLine();
-                addAnItem(item);
-                position++;
-            }
-            else if (current == 2) {
-                listItems();
+        while ((current = in.nextInt()) != 6) {
+            switch (current) {
+                case ADD_ITEM: addAnItem();
+                    break;
+                case LIST_ITEMS: listItems();
+                    break;
+                case DELETE_ITEM: deleteAnItem();
+                    break;
+                case SAVE_LIST: saveListToFile();
+                    break;
+                case LOAD_LIST: loadListFromFile();
+                    break;
+                default:
+                    System.out.println("This is not an acceptable choice, please choose a number 1-6 to proceed with the process.");
             }
         }
+        System.out.println("Good bye!");
 
     }
 
-    public static void addAnItem(String newItem) { //1
-
-        shopList[position] = newItem;
+    public static void addAnItem() { //1
+        System.out.println("Enter item to add: ");
+        shopList[position] = in.next();
+        System.out.println(shopList[position] + " was added, enter your next choice: ");
+        position++;
     }
 
     public static void listItems() { //2
-        for (int i = 0; i < shopList.length; i++) {
-            if (!(shopList[i].equals(" "))) {
-                System.out.println(shopList[i]);
+        for (int i = 0; i < position; i++) {
+            System.out.println(shopList[i]);
+        }
+        System.out.println("Please, enter your next choice: ");
+    }
+
+    public static int countItems (String itemName){
+        int count = 0;
+        for (String i : shopList) {
+            if (i != null && i.equals(itemName)) {
+                count++;
             }
+        }
+        return count;
+    }
+
+    public static void deleteAnItem() {
+        System.out.println("Enter the item you want to delete from the list: ");
+        String item = in.next();
+        switcher(item);
+        System.out.println("Please, enter your next choice: ");
+    }
+
+    public static void delete1 (String itemName) {
+        for (int i = 0; i < position; i++) {
+            if (shopList[i].equals(itemName)) {
+                shopList[i] = null;
+            }
+        }
+        changePositionByOne();
+        System.out.println(itemName + " was removed from the list.");
+    }
+
+    public static void deleteLast (String itemName){
+        //finds the last corresponding item and deletes it, after that exits te loop
+        for (int i = shopList.length-1; i > 0; i--) {
+            if (shopList[i] != null && shopList[i].equals(itemName)) {
+                shopList[i] = null;
+                break;
+            }
+        }
+        changePositionByOne();
+        System.out.println(itemName + " was removed from the list.");
+    }
+
+    public static void deleteAll (String itemName) {
+        //this one deletes every corresponding item
+        for (int i = 0; i < shopList.length-1; i++) {
+            if (shopList[i] != null && shopList[i].equals(itemName)) {
+                shopList[i] = null;
+                changePositionByOne();
+            }
+        }
+        System.out.println("All " + itemName + " were removed from the list.");
+    }
+
+    public static void changePositionByOne() {
+        for (int i = 0; i < position; i++) {
+            if (shopList[i] == null && i+1 != position){
+                shopList[i] = shopList[i+1];
+                shopList[i+1] = null;
+            }
+        }
+        position--;
+    }
+
+    public static void switcher(String itemName) { //3
+        int amount = countItems(itemName);
+        if (amount == 1) {
+            delete1(itemName);
+        }
+        else if (amount > 1) {
+            System.out.println("There is more than 1 element that corresponds to the name " + itemName);
+            System.out.println("Do you want to delete only the last element with this name or all of them? Enter 1 for the first option and 2 for the second: ");
+            int choice = in.nextInt();
+            if (choice == 1) {
+                deleteLast(itemName);
+            }
+            else if (choice == 2) {
+                deleteAll(itemName);
+            }
+        }
+        else {
+            System.out.println("The item you have input does not correspond any item in the list.");
         }
     }
 
-    public void deleteAnItem(String itemName) {
-
+    public static void saveListToFile() { //4
+        String text = Arrays.toString(shopList);
+        try (
+                PrintWriter writer = new PrintWriter(Files.newBufferedWriter(Paths.get("shoppingList.txt"))))
+        {
+            writer.println(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    public static void saveListToFile() {
 
-    }
-    public static void loadListFromFile() {
+    public static void loadListFromFile() { //5
+        String text; //List<String> list = new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get("shoppingList.txt")))
+        {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+               // list.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 /*
 * Scanner sc = new Scanner(new File("myNumbers"));
       while (sc.hasNextLong()) {
           long aLong = sc.nextLong();
       }*/
-      /*В классе String существует масса полезных методов, которые можно применять к строкам (перед именем метода будем указывать тип того значения, которое он возвращает):
-boolean isEmpty() — проверяет, пустая ли строка;
-      */
     }
 }
